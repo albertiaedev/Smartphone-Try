11e34e5eef323256accd8863e598d2a3d5e48f0e
Alberto <albertiae.digital@gmail.com>
2023-01-02 16:03:13 -0400

    Created app.js, where I will load the basic functions for the mobile

---
+++ js/app.js
@@ -0,0 +1,14 @@
+class Cellphone {
+	constructor(model, year, size, screen, cam, ram, storage) {
+		this.model = model;
+		this.year = year;
+		this.size = size;
+		this.screen = screen;
+		this.cam = cam;
+		this.ram = ram;
+		this.storage = storage;
+		this.turnOn = false;
+		this.turnOff = true;
+		this.takePicture = true;
+		this.record = true;
+	}
