803f8bc16e098c21cad20a143df29d1a9dbfb11c
Alberto <albertiae.digital@gmail.com>
2023-01-02 19:59:55 -0400

    Created a function to show mobile info

--- js/childs/childTwo.js
+++ js/childs/childTwo.js
@@ -9,4 +9,20 @@ class SmartphoneTwo extends Cellphone {
 		let extend = document.write(`Your ${this.model} can extend its RAM by ${this.extraRAM}`);
 		return extend;
 	}
+
+	// mobile info
+	smartphoneTwoInfo() {
+		return document.write(
+		`
+			Model: ${this.model}.<br>
+			Year: ${this.year}.<br>
+			Size: ${this.size}.<br>
+			Screen: ${this.screen}.<br>
+			Cam: ${this.cam}.<br>
+			RAM: ${this.ram}.<br>
+			Storage: ${this.storage}.<br>
+			Extra RAM: ${this.extraRAM}.<br>
+		`
+		)
+	}
 }
