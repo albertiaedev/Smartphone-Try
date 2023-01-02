78b0579f125427e8896be067a5013b47bf41a0b6
Alberto <albertiae.digital@gmail.com>
2023-01-02 18:50:34 -0400

    Created a function to show mobile info

--- js/childOne.js
+++ js/childOne.js
@@ -15,4 +15,20 @@ class Smartphone extends Cellphone {
 		let facial = document.write(`Facial Recognition in your ${this.model} has been activated.`);
 		return facial;
 	}
+
+	// mobile info
+	smartphoneInfo() {
+		return document.write(
+		`
+			Model: ${this.model}.<br>
+			Year: ${this.year}.<br>
+			Size: ${this.size}.<br>
+			Screen: ${this.screen}.<br>
+			Cam: ${this.cam}.<br>
+			RAM: ${this.ram}.<br>
+			Storage: ${this.storage}.<br>
+			Extra Cam: ${this.extraCam}.<br>
+		`
+		)
+	}
 }
